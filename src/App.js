4d6139cbd6c0c04e082199213d01ab4719fadd9e
thin nguyen <thinnguyen1888@gmail.com>
2020-11-10 13:13:20 +0700

    add Analog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Clock from "./Components/Clock";
+import Analog from "./Components/Analog";
 
 function App() {
   return (
     <di>
       <Clock />
+      <Analog />
     </di>
   );
 }
