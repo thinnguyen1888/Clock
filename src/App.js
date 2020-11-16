2622b15ea4a244b599b465f643914d580a22be5f
thin nguyen <thinnguyen1888@gmail.com>
2020-11-16 20:43:14 +0700

    update Change Day & Another Clock

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
-import React, { Component } from "react";
 import Clock from "./Components/Clock";
-import Analog from "./Components/Analog";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Clock />
-        <Analog />
-      </div>
-    );
-  }
+const tzDict = {
+  Us: {
+    country: "en-US",
+    tz: { timeZone: "America/New_York" },
+  },
+  Fi: {
+    country: "en-US",
+    tz: { timeZone: "Europe/Istanbul" },
+  },
+  Am: {
+    country: "en-Us",
+    tz: { timeZone: "Europe/London" },
+  },
+};
+
+function App() {
+  return (
+    <div>
+      <Clock id="fi" country={tzDict.Fi.country} timeZone={tzDict.Fi.tz} />
+      <Clock id="us" country={tzDict.Us.country} timeZone={tzDict.Us.tz} />
+      <Clock id="am" country={tzDict.Am.country} timeZone={tzDict.Am.tz} />
+    </div>
+  );
 }
 
 export default App;
